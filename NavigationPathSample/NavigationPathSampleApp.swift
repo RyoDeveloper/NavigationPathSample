b1fa1060606762c36e5479826a7eddece2c7bf46
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2024-12-15 21:39:18 +0900

    Initial Commit

---
+++ NavigationPathSample/NavigationPathSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationPathSampleApp.swift
+//  NavigationPathSample
+//  
+//  https://github.com/RyoDeveloper/NavigationPathSample
+//  Copyright © 2024 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPathSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
